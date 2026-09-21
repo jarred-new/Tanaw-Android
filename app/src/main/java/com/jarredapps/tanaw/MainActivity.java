90f2b6af859073476bbd401597607cebebb34e59
JarredIzKool <reyesgavinjarred@gmail.com>
2026-09-21 13:35:43 +0300

    Layout of Home/Main

--- app/src/main/java/com/jarredapps/tanaw/MainActivity.java
+++ app/src/main/java/com/jarredapps/tanaw/MainActivity.java
@@ -1,12 +1,13 @@
 package com.jarredapps.tanaw;
 
+//import android.R;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
     }
 }
